f0485d7ee530ad02cfaa731d3cba298b99a73b68
Charindu Bandaranayake <charindu.cdb@gmail.com>
2024-10-18 00:08:20 +0530

    Change path new Resume

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -170,7 +170,7 @@ export const BentoGridItem = ({
                 icon={<IoCopyOutline />}
                 position="left"
                 handleClick={handleCopy}
-                otherClasses="!bg-[#161A31]"
+                otherClasses="!bg-[#161A31]" 
               />
 
                {/* Download CV Button */}
