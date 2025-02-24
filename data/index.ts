948c8a7df4e745b8c662337f1aa9b8d678a4a764
Charindu Bandaranayake <charindu.cdb@gmail.com>
2025-02-24 22:57:40 +0530

    Profilepicture changed

--- data/index.ts
+++ data/index.ts
@@ -19,7 +19,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/charindu.svg",
+    img: "/charinduProfilePic.jpg",
   },
   {
     id: 2,
